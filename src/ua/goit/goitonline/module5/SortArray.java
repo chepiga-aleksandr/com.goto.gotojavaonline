d20e276d22bff5c4620e5d26814a471173e010ea
alex <chepiga@ukr.net>
2016-06-22 18:36:53 +0300

    Module2

---
+++ src/ua/goit/goitonline/module5/SortArray.java
@@ -0,0 +1,32 @@
+package ua.goit.goitonline.module5;
+
+public class SortArray {
+    public static void main (String[] args){
+
+        int[] intArray= { 5, 1, 7, -8, 3, 17, 9, 4, -9, 2, 15, 56, 29};
+
+        boolean swapOccurred = true;
+
+        while (swapOccurred) {
+
+            swapOccurred = false;
+
+            for (int i = 0; i < intArray.length - 1; i++) {
+
+                if (intArray[i] < intArray[i + 1]) {
+                    int tempElement = intArray[i];
+
+                    intArray[i] = intArray[i + 1];
+                    intArray[i + 1] = tempElement;
+
+                    swapOccurred = true;
+                }
+            }
+        }
+        for (int element : intArray) {
+            System.out.println(element);
+        }
+    }
+}
+
+
