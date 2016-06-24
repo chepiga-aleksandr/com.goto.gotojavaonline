1eb58f0623eb5cbbf16a1ac7bddcd951590d4333
alex <chepiga@ukr.net>
2016-06-24 15:40:37 +0300

    Add module6

---
+++ src/ua/goit/goitonline/module6/MusicShop/OrderMusicInstrument.java
@@ -0,0 +1,13 @@
+package ua.goit.goitonline.module6.MusicShop;
+
+import java.util.ArrayList;
+
+public class OrderMusicInstrument {
+
+    ArraayList MusicInstrumentList = new ArrayList;
+    MusicInstrumentList.add("Guitar");
+    MusicInstrumentList.add("Piano");
+    MusicInstrumentList.add("Trumplet");
+
+
+}
