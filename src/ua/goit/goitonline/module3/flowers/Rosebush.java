31a4b22660973a721024c527c9df74111419679b
alex <chepiga@ukr.net>
2016-07-02 21:36:23 +0300

    NewVersion

--- src/ua/goit/goitonline/module3/flowers/Rosebush.java
+++ src/ua/goit/goitonline/module3/flowers/Rosebush.java
@@ -1,4 +1,23 @@
 package ua.goit.goitonline.module3.flowers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rosebush {
+    static String type;
+    static List<Rose> roseBush = new ArrayList<>();
+
+    static public String getType() {
+        return type;
+    }
+
+    static public void cutRose() {
+        roseBush.remove(1);
+        System.out.println("Осталось роз: " + roseBush);
+    }
+
+    @Override
+    public String toString() {
+        return "Rosebush contains " + getType();
+    }
 }
