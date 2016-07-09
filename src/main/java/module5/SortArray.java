b98d45523c8bb0fbdce4281927e10287aa7d5d3b
chepiga-aleksandr <chepiga@ukr.net>
2016-07-09 15:33:39 +0300

    Edited module5

--- src/main/java/module5/SortArray.java
+++ src/main/java/module5/SortArray.java
@@ -1,24 +1,16 @@
 package module5;
 
 public class SortArray {
-    public static void main (String[] args){
-
-        int[] intArray= { 5, 1, 7, -8, 3, 17, 9, 4, -9, 2, 15, 56, 29};
-
+    public static void main(String[] args) {
+        int[] intArray = {5, 1, 7, -8, 3, 17, 9, 4, -9, 2, 15, 56, 29};
         boolean swapOccurred = true;
-
         while (swapOccurred) {
-
             swapOccurred = false;
-
             for (int i = 0; i < intArray.length - 1; i++) {
-
                 if (intArray[i] < intArray[i + 1]) {
                     int tempElement = intArray[i];
-
                     intArray[i] = intArray[i + 1];
                     intArray[i + 1] = tempElement;
-
                     swapOccurred = true;
                 }
             }
