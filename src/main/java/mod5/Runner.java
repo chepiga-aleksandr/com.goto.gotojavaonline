b81f2e84a5a06aabde8d517a0d0b7e0cf541043b
chepiga-aleksandr <chepiga@ukr.net>
2016-07-11 22:29:06 +0300

    Added class MinMaxPair, edited accordingly minMaxPair method and Runner

--- src/main/java/mod5/Runner.java
+++ src/main/java/mod5/Runner.java
@@ -1,35 +1,52 @@
 package mod5;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Runner {
     static int size;
-    static int[] array;
-    static Scanner sc = new Scanner(System.in);
 
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Выберите метод создания массива: 1 - автоматический, 2 - ручной ввод");
-        int variant = sc.nextInt();
+        int variant = scanner.nextInt();
         switch (variant) {
             case 1:
                 System.out.println("Ведите размер массива: ");
-                size = sc.nextInt();
-                inputArrayListAuto();
+                size = scanner.nextInt();
+
+                int[] autoArray = inputArrayListAuto();
+
+                autoArray = ArrayUtils.sortCopyArray(autoArray);
+                System.out.println("Отсортированный массив:" + Arrays.toString(autoArray));
+
+                MinMaxPair minMaxPair = ArrayUtils.minMaxPair(autoArray);
+                System.out.println("Максимальное значение массива: " + minMaxPair.getMax() +
+                        ", Минимальное значение массива: " + minMaxPair.getMin());
                 break;
             case 2:
                 System.out.println("Ведите размер массива: ");
-                size = sc.nextInt();
-                inputArrayListManually();
+                size = scanner.nextInt();
+
+                int[] manuallyArray = inputArrayListManually();
+
+                manuallyArray = ArrayUtils.sortCopyArray(manuallyArray);
+                System.out.println("Отсортированный массив:" + Arrays.toString(manuallyArray));
+
+                MinMaxPair minMaxPair1 = ArrayUtils.minMaxPair(manuallyArray);
+                System.out.println("Максимальное значение массива: " + minMaxPair1.getMax() +
+                        ", Минимальное значение массива: " + minMaxPair1.getMin());
                 break;
         }
     }
 
     static int[] inputArrayListAuto() {
         int[] array = new int[size];
+        Random random = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100);
+            array[i] = random.nextInt(50);
         }
         System.out.println("Элементы массива: " + Arrays.toString(array));
         return array;
@@ -39,7 +56,7 @@ public class Runner {
         int[] array = new int[size];
         System.out.println("Введите элементы массива:");
         for (int i = 0; i < array.length; i++) {
-            array[i] = sc.nextInt();
+            array[i] = scanner.nextInt();
         }
         System.out.println("Элементы массива: " + Arrays.toString(array));
         return array;
