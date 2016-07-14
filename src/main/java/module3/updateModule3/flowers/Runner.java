b31d431592e16fa2c5074d493ab6c443448a5555
alex <chepiga@ukr.net>
2016-07-14 10:35:04 +0300

    Update module flowers, class Runner

--- src/main/java/module3/updateModule3/flowers/Runner.java
+++ src/main/java/module3/updateModule3/flowers/Runner.java
@@ -1,26 +1,22 @@
 package module3.updateModule3.flowers;
 
-import module3.updateModule3.flowers.Rosebush;
-
 public class Runner {
 
-    //создаем новый обьект куста роз
-    static Rosebush rosebush = new Rosebush();
+    private static int NUMBER_OF_ROSES_IN_ORDER = 8;
 
-    // задаем количество роз в заказе КОНСТАНТОЙ, к примеру...
-    static int numberOfRosesInOrder = 8;
+    static Rosebush rosebush = new Rosebush();
 
     public static void main(String[] args) {
         // задаем количество роз на кусте
         rosebush.setRoses(20);
 
         System.out.println(rosebush);
-        System.out.println("Количество роз в заказе = " + numberOfRosesInOrder);
+        System.out.println("Количество роз в заказе = " + NUMBER_OF_ROSES_IN_ORDER);
         System.out.println("В магазине (в кусте) осталось " + cutRose() + " роз");
     }
-    // создаем метод по изьятию роз из куста
+
     private static int cutRose() {
-        int balance = rosebush.getRoses() - numberOfRosesInOrder;
+        int balance = rosebush.getRoses() - NUMBER_OF_ROSES_IN_ORDER;
         return balance;
     }
 }
