53fa24dc6645a8ec14928e13c20c1c4055950226
alex <chepiga@ukr.net>
2016-07-15 16:50:04 +0300

    Create module 6

---
+++ src/main/java/module6/MinMaxPair.java
@@ -0,0 +1,16 @@
+package module6;
+
+public class MinMaxPair {
+
+    public int min, max;
+
+    public MinMaxPair(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public String toString() {
+        return "Максимальный элемент массива: " + max + ", Минимальный элемент массива  " + min;
+    }
+}
--- src/main/java/module6/Runner.java
+++ src/main/java/module6/Runner.java
@@ -1,52 +1,78 @@
 package module6;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.zip.DataFormatException;
+
+import static module4.updateDistance.InputFromConsole.sc;
 
 public class Runner {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.println("Выберите метод создания массива: 1 - автоматический, 2 - ручной ввод");
 
-        MusicShop shop = new MusicShop();
-        shop.setGuitars(16);
-        shop.setPianos(2);
-        shop.setTrumplets(7);
-        System.out.println(shop);
+        Scanner sc = new Scanner(System.in);
+        String sw = scanner.next();
+        int ch = 0;
 
-        Map<String, Integer> order = new HashMap<>();
-        order.put("guitar", 7);
-        order.put("piano", 1);
-        order.put("trumplet", 2);
+        try {
+            ch = Integer.parseInt(sw);
+            System.out.println("Вы выбрали вариант - " + ch);
+        } catch (NullPointerException ex) {
+            System.out.println("Вариант выбора может быть только цифрой (порядковым номером варианта создания массива");
+        }
 
-        List<MusicInstrument> instrumentsToRemove = prepareOrder(shop, order);
-        System.out.println("Removed instruments: " + instrumentsToRemove.size());
-        System.out.println(shop);
+        if (ch == 1) {
+            System.out.println("Ведите размер массива: ");
+
+            int[] autoArray = inputArrayListAuto();
+
+            autoArray = ArrayUtils.sortCopyArray(autoArray);
+            System.out.println("Отсортированный массив:" + Arrays.toString(autoArray));
+
+            MinMaxPair minMaxPair = ArrayUtils.minMaxPair(autoArray);
+            System.out.println(minMaxPair);
+        } else if (ch == 2) {
+            System.out.println("Ведите размер массива: ");
+
+            int[] manuallyArray = inputArrayListManually();
+
+            manuallyArray = ArrayUtils.sortCopyArray(manuallyArray);
+            System.out.println("Отсортированный массив:" + Arrays.toString(manuallyArray));
+
+            MinMaxPair minMaxPair = ArrayUtils.minMaxPair(manuallyArray);
+            System.out.println(minMaxPair);
+        }
     }
-    private static List<MusicInstrument> prepareOrder(MusicShop shop, Map<String, Integer> order) {
-        int numberOfGuitarsToRemove = order.get("guitar");
-        int numberOfPianosToRemove = order.get("piano");
-        int numberOfTrampletsToRemove = order.get("trumplet");
 
-        if (shop.getGuitars() < numberOfGuitarsToRemove) throw new IllegalStateException();
-        if (shop.getPianos() < numberOfPianosToRemove) throw new IllegalStateException();
-        if (shop.getTrumplets() < numberOfTrampletsToRemove) throw new IllegalStateException();
+    static int[] inputArrayListAuto() {
+        int[] array = new int[scanner.nextInt()];
+        Random random = new Random();
 
-        shop.setGuitars(shop.getGuitars() - numberOfGuitarsToRemove);
-        shop.setPianos(shop.getPianos() - numberOfPianosToRemove);
-        shop.setTrumplets(shop.getTrumplets() - numberOfTrampletsToRemove);
-
-        List<MusicInstrument> result = new ArrayList<>();
-
-        for (int i = 0; i < numberOfGuitarsToRemove; i++) {
-            result.add(new Guitar());
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(50);
         }
-        for (int i = 0; i < numberOfPianosToRemove; i++) {
-            result.add(new Piano());
+        System.out.println("Элементы массива: " + Arrays.toString(array));
+        return array;
+    }
+
+    static int[] inputArrayListManually() {
+        int[] array = new int[scanner.nextInt()];
+
+        System.out.println("Введите элементы массива:");
+
+        try {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = scanner.nextInt();
+            }
+        } catch (InputMismatchException ex) {
+            System.out.println("Маcсив должен сосотоять из натуральных чисел!");
         }
-        for (int i = 0; i < numberOfTrampletsToRemove; i++) {
-            result.add(new Trumplet());
-        }
-        return result;
+        System.out.println("Элементы массива: " + Arrays.toString(array));
+        return array;
     }
 }
+
