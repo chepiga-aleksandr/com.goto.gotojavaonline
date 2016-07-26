8b47d81c48b2be2e5470a07fa1b8cdbed8f492d6
chepiga-aleksandr <chepiga@ukr.net>
2016-07-26 13:28:42 +0300

    new Practice

---
+++ src/main/java/Practice1/Runner.java
@@ -0,0 +1,12 @@
+package Practice1;
+
+public class Runner {
+    public static void main(String[] args) {
+//        char[] charArray = {'1', '2', '3'};
+        char ch = '8';
+        int intArray = 0;
+//        for (int i = 0; i < charArray.length; i++) {
+        intArray = Character.getNumericValue(ch);
+        System.out.println("Number" + intArray);
+    }
+}
--- src/main/java/module6/MusicShopForModule6/Runner.java
+++ src/main/java/module6/MusicShopForModule6/Runner.java
@@ -1,43 +1,43 @@
-package module6.MusicShopForModule6;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.jar.Manifest;
-
-public class Runner {
-
-        public static void main(String[] args) {
-            MusicShop shop = new MusicShop();
-//создание мапы магазина
-            Map<String, Integer> musicShop = new HashMap<>();
-//наполнили магазин
-            musicShop.put("guitar", 16);
-            musicShop.put("piano", 23);
-            musicShop.put("trumpet", 17);
-            System.out.println("В наличии в магазине:" + musicShop);
-
-            System.out.println(shop);
-//          return shop;
-//          return shop;
-//          prepareInstruments();
-        }
-        public List<MusicalInstrument> prepareInstruments(Map<String, Integer> order) {
-
-          order = new HashMap<>();
-          order.put("guitar", 6);
-          order.put("piano", 2);
-          order.put("trumpet", 7);
-
-            int numberOfGuitarsToRemove = order.get("guitar");
-            int numberOfPianosToRemove = order.get("piano");
-            int numberOfTrumpetsToRemove = order.get("trumpet");
-
-            shop.setGuitars(shop.getGuitars() - numberOfGuitarsToRemove);
-            shop.setPianos(shop.getPiano() - numberOfPianosToRemove);
-            shop.setTrumpets(shop.getTrumpets() - numberOfTrumpetsToRemove);
-
-            System.out.println("Заказано:" + order);
-
-        }
-    }
+//package module6.MusicShopForModule6;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.jar.Manifest;
+//
+//public class Runner {
+//
+//        public static void main(String[] args) {
+//            MusicShop shop = new MusicShop();
+////создание мапы магазина
+//            Map<String, Integer> musicShop = new HashMap<>();
+////наполнили магазин
+//            musicShop.put("guitar", 16);
+//            musicShop.put("piano", 23);
+//            musicShop.put("trumpet", 17);
+//            System.out.println("В наличии в магазине:" + musicShop);
+//
+//            System.out.println(shop);
+////          return shop;
+////          return shop;
+////          prepareInstruments();
+//        }
+// //       public List<MusicalInstrument> prepareInstruments(Map<String, Integer> order) {
+//
+//          order = new HashMap<>();
+//          order.put("guitar", 6);
+//          order.put("piano", 2);
+//          order.put("trumpet", 7);
+//
+//            int numberOfGuitarsToRemove = order.get("guitar");
+//            int numberOfPianosToRemove = order.get("piano");
+//            int numberOfTrumpetsToRemove = order.get("trumpet");
+//
+//            shop.setGuitars(shop.getGuitars() - numberOfGuitarsToRemove);
+//            shop.setPianos(shop.getPiano() - numberOfPianosToRemove);
+//            shop.setTrumpets(shop.getTrumpets() - numberOfTrumpetsToRemove);
+//
+//            System.out.println("Заказано:" + order);
+//
+//        }
+//    }
