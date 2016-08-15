47f97467ac312d32ef1b2ba5d231ee30e89bdc90
chepiga-aleksandr <chepiga@ukr.net>
2016-08-15 13:49:53 +0300

    Added module11

--- src/main/java/module10/Runner.java
+++ src/main/java/module10/Runner.java
@@ -3,8 +3,6 @@ package module10;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
-import module10.Cryptographer;
-import module10.ReadFile;
 
 public class Runner {
     public Runner() {
@@ -12,39 +10,24 @@ public class Runner {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Введите Ваше сообщение для шифрования длинной не более 20 символов");
+
         Scanner scanner = new Scanner(System.in);
+
         String inputMessage = Cryptographer.encrypt(scanner.nextLine());
 
-        try {
-            FileOutputStream outputMessage = new FileOutputStream("D://module10//text.txt");
-            Throwable var4 = null;
+        try (FileOutputStream outputMessage = new FileOutputStream("D://module10//text.txt")) {
 
-            try {
-                byte[] buffer = inputMessage.getBytes();
-                outputMessage.write(buffer, 0, buffer.length);
-            } catch (Throwable var14) {
-                var4 = var14;
-                throw var14;
-            } finally {
-                if(outputMessage != null) {
-                    if(var4 != null) {
-                        try {
-                            outputMessage.close();
-                        } catch (Throwable var13) {
-                            var4.addSuppressed(var13);
-                        }
-                    } else {
-                        outputMessage.close();
-                    }
-                }
+            byte[] buffer = inputMessage.getBytes();
+            outputMessage.write(buffer, 0, buffer.length);
 
-            }
-        } catch (IOException var16) {
-            System.out.println(var16.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
 
         String outputMessage1 = Cryptographer.decrypt(ReadFile.read("D://module10//text.txt"));
+
         System.out.println("Зашифрованный - " + inputMessage);
+
         System.out.println("Расшифрованный -  " + outputMessage1);
     }
 }
