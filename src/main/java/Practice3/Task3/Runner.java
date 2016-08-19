3d6c807e79b8b2c8c55abed3c7e1d39a5b5a2904
Администратор <chepiga@ukr.net>
2016-08-19 14:07:47 +0300

    added Practice3.3

--- src/main/java/Practice3/Task3/Runner.java
+++ src/main/java/Practice3/Task3/Runner.java
@@ -1,16 +1,50 @@
 package Practice3.Task3;
 
+import java.util.ArrayDeque;
+
 public class Runner {
-
-
     public static void main(String[] args) {
+
+        String formula = "825*+132*+4-/";
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < formula.length(); i++) {
+
+            char element = formula.charAt(i);
+            if (Character.isDigit(element)) {
+
+                stack.addFirst(Character.getNumericValue(element));
+
+            } else {
+
+                int a = stack.pollFirst();
+                int b = stack.pollFirst();
+                int result;
+
+                if (element == '+') {
+
+                    result = a + b;
+                    stack.addFirst(result);
+                }
+                if (element == '-') {
+
+                    result = b - a;
+                    stack.addFirst(result);
+                }
+                if (element == '*') {
+
+                    result = a * b;
+                    stack.addFirst(result);
+                }
+                if (element == '/') {
+
+                    result = b / a;
+                    stack.addFirst(result);
+                }
+            }
+        }
+        System.out.println(stack.getFirst());
     }
 }
 
-
-
-
-//Дано арифметическое выражение в виде Обратной польской записи.
-//Необходимо посчитать значение выражения и вернуть его.
-//Выражение состоит только из целых числе и операций +, -, *, /.
-//Гарантируется, что результать - тоже целое число.
