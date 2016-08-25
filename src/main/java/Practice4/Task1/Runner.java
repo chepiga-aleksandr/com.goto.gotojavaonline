833065aae0b62cb8260513ebb9e561eee3ec535a
Администратор <chepiga@ukr.net>
2016-08-25 18:17:51 +0300

    Added Practice 4.1

--- src/main/java/Practice4/Task1/Runner.java
+++ src/main/java/Practice4/Task1/Runner.java
@@ -1,22 +1,17 @@
 package Practice4.Task1;
 
 public class Runner {
-
     public static void main(String[] args) {
-
         int n = 10;
         int w = 1;
         int y = 2;
         int r = 3;
         int count = 0;
-        int l = 0;
-        int i = 0;
-        int j = 0;
-        int g = 0;
 
-        for (i = 0; i < 100; i++) {
-            l = r * i;
-            if (l == n){
+        for (int i = 0; ; i++) {
+            int l = r * i;
+
+            if (l == n) {
                 count++;
                 break;
             }
@@ -25,27 +20,23 @@ public class Runner {
                 break;
             }
 
-            for (j = 0; j < 100; j++) {
+            for (int j = 0; ; j++) {
+                l = r * i + y * j;
 
-               l = r * i + y * j;
-                if (l == n){
+                if (l == n) {
                     count++;
                     break;
                 }
+
                 if (l > n) {
                     break;
                 }
 
-                for (g = 0; g < 100; g++) {
+                for (int g = 0; ; g++) {
+                    l = r * i + y * j + w * g;
 
                     if (l == n) {
                         count++;
-                        break;
-                    }
-                    l = r * i + y * j + w * g;
-                    if (l == n) {
-                        count++;
-//                        l = 0;
                         break;
                     }
                 }
