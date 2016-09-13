5d5027b453a1e1cf53955b3e9a2c21c9a162f677
alex <chepiga@ukr.net>
2016-09-13 11:18:35 +0300

    Updated Practice1 Task1

--- src/main/java/Practice1/Task1/Runner.java
+++ src/main/java/Practice1/Task1/Runner.java
@@ -3,10 +3,18 @@ package Practice1.Task1;
 public class Runner {
     public static void main(String[] args) {
         char[] charArray = {'1', '2', '3'};
-        int intArray = 0;
-        for (int i = 0; i < charArray.length; i++) {
-            intArray = Character.getNumericValue(charArray[i]);
-            System.out.print(intArray);
+
+        StringBuilder charToString = new StringBuilder();
+
+        try {
+            for (int i = 0; i < charArray.length; i++) {
+
+                charToString.append(charArray[i]);
+            }
+            System.out.println(Integer.parseInt(charToString.toString()));
+
+        } catch (Exception e) {
+            System.out.println("Входная строка содержит некорректные символы");
         }
     }
 }
