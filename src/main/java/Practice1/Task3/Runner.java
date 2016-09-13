703d0ee409b306c02445ecc5e7e29f1405a5466d
alex <chepiga@ukr.net>
2016-09-13 13:29:40 +0300

    Updated Practice1 Task3

--- src/main/java/Practice1/Task3/Runner.java
+++ src/main/java/Practice1/Task3/Runner.java
@@ -3,14 +3,15 @@ package Practice1.Task3;
 public class Runner {
 
     public static void main(String[] args) {
-        int[] arr = {2, 4, 8, 2, -4, -6, -8};
-        for (int i = 0; i <= arr.length; i++) {
-            try {
-                if ((arr[i] % 2) != 0) {
-                    System.out.print("Number -" + arr[i] + ", its index :" + i);
-                    break;
-                }
-            } catch (Exception e) {
+        int[] arr = {2, 6, 8, 2, -4, -6, -8};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if ((arr[i] % 2) != 0) {
+                System.out.println("Number -" + arr[i] + ", its index :" + i);
+                break;
+            }
+            if (i == arr.length-1) {
                 System.out.println(-1);
             }
         }
