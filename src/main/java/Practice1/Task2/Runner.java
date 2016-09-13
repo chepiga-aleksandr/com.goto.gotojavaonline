1aeb8610036405dd944d47e552b68e095e8d0c96
alex <chepiga@ukr.net>
2016-09-13 14:03:48 +0300

    Updated Practice1 Task2

--- src/main/java/Practice1/Task2/Runner.java
+++ src/main/java/Practice1/Task2/Runner.java
@@ -3,10 +3,10 @@ package Practice1.Task2;
 public class Runner {
 
     public static void main(String[] args) {
-        int a = 666;
+        int a = 876;
         int s = 0;
-        do s = s + (a % 10);
-        while ((a=a/10)!= 0);
+        do s += (a % 10);
+        while ((a /= 10) != 0);
         System.out.println(s + " ");
     }
 }
