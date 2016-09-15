ce6ddc8b523d6f10ce523ac9a7082ecfb4ea1e14
alex <chepiga@ukr.net>
2016-09-15 15:19:14 +0300

    Updated Practice1 Task5

--- src/main/java/Practice1/Task5/Runner.java
+++ src/main/java/Practice1/Task5/Runner.java
@@ -4,64 +4,40 @@ import java.util.Arrays;
 
 public class Runner {
     public static void main(String[] args) {
+
         int[][] matrix = {
                 {1, 2, 3, 4, 5},
-                {6, 7, 8, 9 , 10},
+                {6, 7, 8, 9, 10},
                 {11, 12, 13, 14, 15},
-                {16, 17, 18, 19, 20},
-                {21, 22, 23, 24, 25}
+                {12, 13, 14, 15, 16},
+                {17, 18, 19, 20, 21},
         };
-        int[] ints = goIteratorGo(matrix);
 
-        for (int anInt : ints) {
-            System.out.println(anInt);
-        }
-    }
+        int size = matrix.length;
 
-    private static int[] goIteratorGo(int[][] input) {
-        int[] result = new int[input.length * input[0].length];
-        int sizeX = input[0].length - 1;
-        int sizeY = input.length - 1;
-        int startX = 0;
-        int startY = 0;
-        int count = 1;
+        int[] matrixNew = new int[size * size];
+
         int index = 0;
-        while (index < result.length) {
-            if (count == 1) {
-                for (int i = startX; i <= sizeX; i++) {
-                    result[index] = input[startY][i];
-                    index++;
-                }
-                startY++;
-            }
-            if (count == 2) {
-                for (int i = startY; i <= sizeY; i++) {
-                    result[index] = input[i][sizeX];
-                    index++;
-                }
-                sizeX--;
-            }
-            if (count == 3) {
-                for (int i = sizeX; i >= startX; i--) {
-                    result[index] = input[sizeY][i];
-                    index++;
-                }
-                sizeY--;
-            }
-            if (count == 4) {
-                for (int i = sizeY; i >= startY; i--) {
-                    result[index] = input[i][startX];
-                    index++;
-                }
-                startX++;
-            }
+        int x, y;
 
-            if (count < 4) {
-                count++;
-            } else {
-                count = 1;
+        for (y = 0; y < size * size; y++) {
+
+            for (x = 0; x < size; x++) {
+
+                matrixNew[index] = matrix[x][y];
+                index++;
+            }
+            y++;
+            if (index == size * size) {
+                break;
+            }
+            for (x = size - 1; x >= 0; x--) {
+
+                matrixNew[index] = matrix[x][y];
+                index++;
             }
         }
-        return result;
+        System.out.println(Arrays.toString(matrixNew));
     }
 }
+
