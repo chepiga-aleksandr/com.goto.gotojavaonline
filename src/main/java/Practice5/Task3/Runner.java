c9709c1dc8f051f03d535b06d95c96de2299ee5e
Администратор <chepiga@ukr.net>
2016-09-25 21:30:29 +0300

    Merge branch 'master' of https://github.com/chepiga-aleksandr/com.goto.gotojavaonline
    # Conflicts:
    #	src/main/java/Practice3/Task5/BinaryHeap.java

--- src/main/java/Practice5/Task3/Runner.java
+++
@@ -1,4 +0,0 @@
-package Practice5.Task3;
-
-public class Runner {
-}
---
+++ src/main/java/Practice5/Task3/Tree.java
@@ -0,0 +1,10 @@
+package Practice5.Task3;
+
+public class Tree {
+
+    private Node root;
+
+    public Tree() {
+        this.root = null;
+    }
+}
