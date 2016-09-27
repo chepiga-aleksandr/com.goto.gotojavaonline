c455ab366c08fe45b686096e7671983876a38b3e
alex <chepiga@ukr.net>
2016-09-27 11:43:34 +0300

    Practice5 Task1

--- src/main/java/Practice5/Task1/Runner.java
+++ src/main/java/Practice5/Task1/Runner.java
@@ -6,28 +6,32 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        String inputValue = "123456789";
+        String inputValue = "123456";
 
         String substring1, substring2 = null;
+        int length = inputValue.length();
+
 
         TreeSet<Integer> period = new TreeSet<>();
 
-        for (int i = 0; i <= inputValue.length()/2; i++) {
+        for (int i = 0; i <= length / 2; i++) {
 
-            for (int j = 1; j < inputValue.length()-i; j++) {
+            for (int j = 1; j < length - i; j++) {
 
                 substring1 = inputValue.substring(0, i + 1);
+                if ((substring1.length() * j + substring1.length()) > inputValue.length()) {
+                    break;
+                }
 
-                if (substring1.length()*2 < inputValue.length()) {
+                if (substring1.length() * 2 <= length) {
                     substring2 = inputValue.substring(substring1.length() * j, substring1.length() * j + substring1.length());
                 }
-    //            String str = substring2;
-             if (substring1.equals(substring2)) {
-            period.add(substring1.length());
-             }
+
+                if (substring1.equals(substring2)) {
+                    period.add(inputValue.length());
+                }
             }
         }
-//       System.out.println(period.first());
     }
 }
 
