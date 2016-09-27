57b0b0e9fba3fe63db8fe1935143ee1a0e7b7542
alex <chepiga@ukr.net>
2016-09-27 17:01:35 +0300

    Practice5 Task1

--- src/main/java/Practice5/Task1/Runner.java
+++ src/main/java/Practice5/Task1/Runner.java
@@ -1,36 +1,43 @@
 package Practice5.Task1;
 
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class Runner {
 
     public static void main(String[] args) {
 
-        String inputValue = "123456";
+        String inputValue = "abcabc";
 
-        String substring1, substring2 = null;
+        String substring1, substring2;
         int length = inputValue.length();
 
-
-        TreeSet<Integer> period = new TreeSet<>();
+        int[] period = new int[1];
 
         for (int i = 0; i <= length / 2; i++) {
 
             for (int j = 1; j < length - i; j++) {
 
                 substring1 = inputValue.substring(0, i + 1);
-                if ((substring1.length() * j + substring1.length()) > inputValue.length()) {
+
+                if ((substring1.length() * j + substring1.length()) >= length || substring1.length() > length - substring1.length()*2 ) {
                     break;
+                } else {
+                substring2 = inputValue.substring(substring1.length() * j, substring1.length() * j + substring1.length());
                 }
 
-                if (substring1.length() * 2 <= length) {
-                    substring2 = inputValue.substring(substring1.length() * j, substring1.length() * j + substring1.length());
-                }
-
-                if (substring1.equals(substring2)) {
-                    period.add(inputValue.length());
+                if (!substring1.equals(substring2)) {
+                    period[0] = 1;
+                    break;
+                } else {
+                   period[0] = substring1.length();
                 }
             }
+        }
+        if (period.length == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(period[0]);
         }
     }
 }
