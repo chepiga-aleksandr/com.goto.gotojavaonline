09e582064cc1e037703dacb3a3377d5cd477b9af
alex <chepiga@ukr.net>
2016-09-28 14:20:41 +0300

    Practice2 Task2

--- src/main/java/Practice2/Task2/Runner.java
+++ src/main/java/Practice2/Task2/Runner.java
@@ -4,9 +4,16 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        int a = 4, b = 7;
-        int numbers = a % 2 + b % 2;
-        int result = a / 2 + b / 2;
+        double a = 4, b = 7;
+        if ( a > 0 && b > 0) {
+            Calculate(a, b);
+        } else {
+            System.out.println("Входные данные некорректны");
+        }
+    }
+    private static void Calculate(double a, double b) {
+        double numbers = a % 2 + b % 2;
+        double result = a / 2 + b / 2;
         result = result + numbers / 2;
         System.out.println(result);
     }
