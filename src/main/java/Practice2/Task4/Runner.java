212948fb15e2daeb450d3d3a6cd5f72408ade186
alex <chepiga@ukr.net>
2016-09-29 11:07:53 +0300

    Practise2Task4

--- src/main/java/Practice2/Task4/Runner.java
+++ src/main/java/Practice2/Task4/Runner.java
@@ -4,19 +4,20 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        int x = convertStringForInt("gaga");
+        int x = convertStringForInt("GAGA");
         System.out.println(x);
     }
 
     public static int convertStringForInt(String stringInput) {
 
-        String BASEOFLETTERS = "abcdefghi";
+        String BASEOFLETTERS = "abcdefghij";
 
         StringBuilder numbers = new StringBuilder();
 
-        int result = 0;
+        int result;
+
         for (int i = 0; i < stringInput.length(); i++) {
-            numbers.append(BASEOFLETTERS.indexOf(stringInput.charAt(i)));
+            numbers.append(BASEOFLETTERS.indexOf(stringInput.toLowerCase().charAt(i)));
         }
 
         result = Integer.parseInt(numbers.toString());
