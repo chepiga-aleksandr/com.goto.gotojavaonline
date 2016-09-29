722340edb8acd6a42119e00feb4935a3401eeb64
alex <chepiga@ukr.net>
2016-09-29 16:51:38 +0300

    Practice2Task4

--- src/main/java/Practice2/Task4/Runner.java
+++ src/main/java/Practice2/Task4/Runner.java
@@ -2,28 +2,46 @@ package Practice2.Task4;
 
 public class Runner {
 
-    public static void main(String[] args) {
+    private String BASEOFLETTERS = "abcdefghij";
+    private boolean checkFlag = true;
 
-        int x = convertStringForInt("GAGA");
-        System.out.println(x);
+    public boolean checkData(String stringInput) {
+        boolean isContains = true;
+        for (int i = 0; i < stringInput.length(); i++) {
+            String temp = stringInput.valueOf(stringInput.toLowerCase().charAt(i));
+
+            if (!isContains == BASEOFLETTERS.contains(temp)) {
+                checkFlag = false;
+                System.out.println("Входные данные некорректны");
+                break;
+            } else {
+                checkFlag = true;
+            }
+        }
+        return checkFlag;
     }
 
-    public static int convertStringForInt(String stringInput) {
-
-        String BASEOFLETTERS = "abcdefghij";
+    public int convertStringForInt(String stringInput) {
 
         StringBuilder numbers = new StringBuilder();
-
         int result;
 
         for (int i = 0; i < stringInput.length(); i++) {
             numbers.append(BASEOFLETTERS.indexOf(stringInput.toLowerCase().charAt(i)));
         }
-
         result = Integer.parseInt(numbers.toString());
-
         return result;
     }
+
+    public static void main(String[] args) {
+
+        Runner runner = new Runner();
+
+        String inputData = "gf";
+
+        if (runner.checkData(inputData)) {
+            int x = runner.convertStringForInt(inputData);
+            System.out.println(x);
+        }
+    }
 }
-
-
