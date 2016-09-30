ae420b5c1f1d47acb4b8e6895246b5742e2b3e65
alex <chepiga@ukr.net>
2016-09-30 10:32:45 +0300

    Practice2Task6

--- src/main/java/Practice2/Task6/Runner.java
+++ src/main/java/Practice2/Task6/Runner.java
@@ -2,18 +2,18 @@ package Practice2.Task6;
 
 public class Runner {
 
-    public static String replacedBit(String inputString, int x) {
+    public static String replacedBit(String inputString, int positionNumber, char symbol) {
 
         StringBuilder number = new StringBuilder(inputString);
 
-        number.setCharAt(x, '0');
+        number.setCharAt(positionNumber, symbol);
 
         return number.toString();
     }
 
     public static void main(String[] args) {
 
-        String result = replacedBit("11111111", 3);
+        String result = replacedBit("11111111", 3, '0');
 
         System.out.println(result);
     }
