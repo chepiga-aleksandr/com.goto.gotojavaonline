a79e35dcaf69faf6dd67fbb03e172312da192b97
alex <chepiga@ukr.net>
2016-10-13 15:59:02 +0300

    WorkTask

---
+++ src/main/java/SemaphoreIgor/SemaphoreInterface.java
@@ -0,0 +1,15 @@
+package SemaphoreIgor;
+
+public interface SemaphoreInterface {
+
+    public void acquire() throws InterruptedException;
+
+    public void acquire(int permits) throws InterruptedException;
+
+    public void release() throws InterruptedException;
+
+    public void release(int permits) throws InterruptedException;
+
+    public int getAvailablePermits();
+
+}
